c40c5f3967e0fe4c76d3f1e198c1dce2d59d2a2b
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-09-03 13:31:36 +0100

    fix

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,29 +1,29 @@
-import { Container, TableTransactions } from './TransactionHistory.styled';
+import { TableTransactions } from './TransactionHistory.styled';
 
 export const TransactionHistory = ({ items }) => {
   return (
     <TableTransactions>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          {items.map(transaction => {
-            return (
-              <TransactionItem
-                key={transaction.id}
-                type={transaction.type}
-                amount={transaction.amount}
-                currency={transaction.currency}
-              />
-            );
-          })}
-        </tbody>
-      </TableTransactions>
+      <tbody>
+        {items.map(transaction => {
+          return (
+            <TransactionItem
+              key={transaction.id}
+              type={transaction.type}
+              amount={transaction.amount}
+              currency={transaction.currency}
+            />
+          );
+        })}
+      </tbody>
+    </TableTransactions>
   );
 };
 
